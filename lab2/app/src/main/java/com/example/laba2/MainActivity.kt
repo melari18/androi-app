12609a946d29b134f1bb5a2e90a3679187578995
Мельничук Арина <melnichuk.arina05@mail.ru>
2026-09-18 15:02:14 +1000

    Переструктурировал: lab1 и lab2 в одном репозитории

---
+++ lab2/app/src/main/java/com/example/laba2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.laba2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import android.widget.EditText
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val textView = findViewById<TextView>(R.id.textView)
+        val editText = findViewById<EditText>(R.id.editTextText)
+        val button = findViewById<Button>(R.id.button)
+        textView.text = "" // чтобы при старте не был виден
+        button.setOnClickListener { textView.text = "Привет, " + editText.text + "!" }
+    }
+}
